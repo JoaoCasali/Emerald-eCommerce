f82169ee4620df7f4d0ff97a0d1e9969a9c33c08
João Vitor <joaovitorcasali@gmail.com>
2023-02-28 20:21:43 -0300

    cadastro de cliente

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,7 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Cliente(AbstractUser):
-    
-    pass
-
-
+    cpf_cnpj = models.CharField(max_length=14, default='00000000000000')
