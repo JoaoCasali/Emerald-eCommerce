1e51afe6407d27d1716c5e8bb25bf7eb1f0df297
Felipe Weiss <felipe.weiss@proway.treina>
2023-03-01 18:56:40 -0300

    update

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Cliente(AbstractUser):
+    cpf_cnpj = models.CharField(max_length=14, default='00000000000000')
     
-    pass
 
 
