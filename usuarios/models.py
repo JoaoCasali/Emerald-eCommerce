730f9cf9977fcb6f3b3d221c365282556551a452
Alejandro Silveira Ramos <alejandro.ramos@proway.treina>
2023-03-03 19:24:53 -0300

    oi

--- usuarios/models.py
+++ usuarios/models.py
@@ -2,4 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Cliente(AbstractUser):
+    id = models.AutoField(primary_key=True, null=False)
     cpf_cnpj = models.CharField(max_length=14, default='00000000000000')
