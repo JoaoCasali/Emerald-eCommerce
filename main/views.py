1e5d95c5c391b7e4902de9108d939b91bf365cec
João Vitor <joaovitorcasali@gmail.com>
2023-03-03 19:23:04 -0300

    main

--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 
-# Create your views here.
+def Home(request):
+    return render(request, 'home.html')
+
