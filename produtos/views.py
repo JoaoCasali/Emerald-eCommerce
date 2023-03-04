f61e894dd06cfe07fa84ac02ed3ec253f49edee0
Melissa Konig <melissa.konig@proway.treina>
2023-03-03 21:25:37 -0300

    alterações listagem

--- produtos/views.py
+++ produtos/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Produto
 
-# Create your views here.
+def Listagem(request):
+    if request.method == "GET":
+        return render(request, 'listagem.html')
