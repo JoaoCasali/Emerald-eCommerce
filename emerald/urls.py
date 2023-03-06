0b3c44abddf6ef7fe2e96a05ee9b2eed16823ab2
Felipe Weiss <felipe.weiss@proway.treina>
2023-03-06 20:11:36 -0300

    pfv da certo

--- emerald/urls.py
+++ emerald/urls.py
@@ -10,3 +10,9 @@ urlpatterns = [
     path('Produtos/', include('produtos.urls')),
     path('', include('main.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+
+handler404 = "main.views.PageNotFound"
