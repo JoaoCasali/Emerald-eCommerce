0d67312472650b9e7890684ebf6e747c2f927a1e
Felipe Weiss <felipe.weiss@proway.treina>
2023-03-06 18:27:26 -0300

    update

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 def Home(request):
     return render(request, 'home.html')
 
+def PageNotFound(request, exception):
+    return render(request, 'notFound.html')
--- produtos/urls.py
+++ produtos/urls.py
@@ -4,5 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
+
+    path('Cadastro/', views.CadastroProduto, name='cadastroProduto'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
