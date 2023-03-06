c8257842d4d11b1c97c6d440144be9eb64626da2
João Vitor <joaovitorcasali@gmail.com>
2023-03-06 20:44:00 -0300

    Url

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('list/', views.product_list, name = "listagem")
+    path('list/', views.product_list, name = "listagem"),
     path('Cadastro/', views.Cadastro, name='cadastroProduto'),
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
