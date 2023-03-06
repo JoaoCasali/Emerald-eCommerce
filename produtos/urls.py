accc3f9bd1544cf8b8885de653581125c57841dd
Felipe Weiss <felipe.weiss@proway.treina>
2023-03-06 20:43:57 -0300

    update

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('list/', views.product_list, name = "listagem")
+    path('list/', views.product_list, name = "listagem"),
     path('Cadastro/', views.Cadastro, name='cadastroProduto'),
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
